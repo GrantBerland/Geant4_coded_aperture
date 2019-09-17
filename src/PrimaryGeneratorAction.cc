670888bc1ebf11276851443c771da61a97d32445
GrantBerland <grant.berland.gb@gmail.com>
2019-09-16 18:34:34 -0600

    added angled infinitely far distribution type to PrimaryGenerator, case 3. Also added macro file to run that case

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -100,6 +100,8 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   G4double narrowingOffset;
   G4double detectorSize;
 
+  detectorSize  = 40*2;
+
   do{
     energy = -(fE0-50) * std::log(1 - G4UniformRand()) * keV;
   } while(energy < 50.*keV);
@@ -118,7 +120,6 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 		break;
 
 	case 1: // point source, infinitely far
-		detectorSize  = 40*2;
 		x = G4UniformRand()*detectorSize - detectorSize/2.; 
 		x *= mm;
 		y = G4UniformRand()*detectorSize - detectorSize/2.;
@@ -137,6 +138,24 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 		
 		xDir = yDir = 0;
 		zDir = 1;
+		break;
+
+	case 3: // Rotated plane, infinitely far away
+		theta = -30. * fPI/180.; // pi/18 rad = 10 deg
+		
+		x = G4UniformRand()*detectorSize - detectorSize/2.; 
+		x *= mm;
+
+		x -= 5*cm;
+
+		y = G4UniformRand()*detectorSize - detectorSize/2.;
+		y *= mm;
+		z = -20.*cm;
+
+		xDir = std::cos(theta) + std::sin(theta); 
+		yDir = 0; 
+		zDir = -std::sin(theta) + std::cos(theta);
+
 		break;
 	default:
 		throw std::invalid_argument("Choose distribution type!");
